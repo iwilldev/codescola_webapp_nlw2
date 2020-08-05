d0772d69bfb3a6110ae329d344f2d4166f2eaa24
William Goncalves <euwilliamgoncalves@gmail.com>
2020-08-05 14:56:36 -0300

    Padronizando estilização e props do PageHeader

--- src/pages/TeacherForm/index.tsx
+++ src/pages/TeacherForm/index.tsx
@@ -1,8 +1,13 @@
 import React from 'react';
+import PageHeader from '../../components/PageHeader';
+
+import './styles.css';
 
 function TeacherForm() {
     return (
-        <h1>Teachers Form</h1>
+        <div id="page-teacher-form" className="container">
+            <PageHeader title="Que bom poder contar contigo!"/>
+        </div>
     )
 }
 
--- src/pages/TeacherList/index.tsx
+++ src/pages/TeacherList/index.tsx
@@ -1,8 +1,28 @@
 import React from 'react';
+import PageHeader from '../../components/PageHeader';
+
+import './styles.css';
 
 function TeacherList() {
     return (
-        <h1>Teachers List</h1>
+        <div id="page-teacher-list" className="container">
+            <PageHeader title="Conheça os professores disponíveis">
+                <form id="search-teachers">
+                    <div className="input-block">
+                        <label htmlFor="subject">Matéria</label>
+                        <input type="text" id="subject" />
+                    </div>
+                    <div className="input-block">
+                        <label htmlFor="week_day">Dia da Semana</label>
+                        <input type="text" id="week_day" />
+                    </div>
+                    <div className="input-block">
+                        <label htmlFor="time">Horário</label>
+                        <input type="text" id="time" />
+                    </div>
+                </form>
+            </PageHeader>
+        </div>
     )
 }
 
