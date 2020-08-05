cb727ba8768cae39e9dd12ed96631eb741ed1f3a
William Goncalves <euwilliamgoncalves@gmail.com>
2020-08-05 14:18:51 -0300

    Revisando estilos na Landing Page

--- src/pages/Landing/index.tsx
+++ src/pages/Landing/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 import logoImg from '../../assets/images/logo.svg';
 import landingImg from '../../assets/images/landing.svg';
@@ -30,14 +31,14 @@ function Landing() {
                 />
 
                 <div className="buttons-container">
-                    <a href="" className="study">
+                    <Link to="/study" className="study">
                         <img src={studyIcon} alt="Estudar"/>
                         Estudar
-                    </a>
-                    <a href="" className="give-classes">
+                    </Link>
+                    <Link to="/give-classes" className="give-classes">
                         <img src={giveClassesIcon} alt="Dar Aulas"/>
                         Dar Aulas
-                    </a>
+                    </Link>
                 </div>
 
                 <span className="total-connections">
